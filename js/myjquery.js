9ea4c99be73d5ae5140a60657f6664a790062a22
Damien Lowe <ashleydamienlowe@gmail.com>
2017-11-09 18:31:03 +1100

    Move info storage

--- js/myjquery.js
+++ js/myjquery.js
@@ -1,18 +1,10 @@
-// const render = function () {
-//
-// };
-//
-// console.log('myq')
-
-
-
-
-
 let player1 = true;
 let squareEmpty = true;
 let turnCounter = 0;
 let checkWinX = false;
 let checkWinO = false;
+let checkDraw = false;
+let roundInPlay = true;
 
 //Set all squares to by "empty"
 const empty = function (el) {
@@ -22,78 +14,116 @@ const empty = function (el) {
 };
 
 const checkWinOrDraw = function () {
-  if (turnCounter === 9) {
-    console.log("DRAW!");
-    checkDraw = true;
-  };
   //Check Row1-X
-  if ($(".row1.playedX, .row2.playedX, .row3.playedX, .col1.playedX, .col2.playedX, .col3.playedX  ").length === 3) {
-    // console.log("You have won!");
+  if ($(".row1.playedX").length === 3) {
     checkWinX = true;
+    roundInPlay = false;
   };
   //Check Row1-0
-  if ($(".row1.playedO, .row2.playedO, .row3.playedO, .col1.playedO, .col2.playedO, .col3.playedO").length === 3) {
+  if ($(".row1.playedO").length === 3) {
     checkWinO = true;
+    roundInPlay = false;
   };
-  // //Check Row2-X
-  // if ($(".row2.playedX").length === 3) {
-  //   // console.log("You have won!");
-  //   checkWinX = true;
-  // };
-  // //Check Row2-0
-  // if ($(".row2.playedO").length === 3) {
-  //   checkWinO = true;
-  // };
-  // //Check Row3-X
-  // if ($(".row3.playedX").length === 3) {
-  //   // console.log("You have won!");
-  //   checkWinX = true;
-  // };
-  // //Check Row3-0
-  // if ($(".row3.playedO").length === 3) {
-  //   checkWinO = true;
-  // };
+  //Check Row2-X
+  if ($(".row2.playedX").length === 3) {
+    // console.log("You have won!");
+    checkWinX = true;
+    roundInPlay = false;
+  };
+  //Check Row2-0
+  if ($(".row2.playedO").length === 3) {
+    checkWinO = true;
+    roundInPlay = false;
+  };
+  //Check Row3-X
+  if ($(".row3.playedX").length === 3) {
+    // console.log("You have won!");
+    checkWinX = true;
+    roundInPlay = false;
+  };
+  //Check Row3-0
+  if ($(".row3.playedO").length === 3) {
+    checkWinO = true;
+    roundInPlay = false;
+  };
 
-  // //Column 1 X
-  // if ($(".col1.playedX").length === 3) {
-  //   checkWinX = true;
-  // // };
-  // //Column 1 O
-  // if ($(".col1.playedO").length === 3) {
-  //   checkWinO = true;
-  // };
-  // // //Column 2 X
-  // if ($(".col2.playedX").length === 3) {
-  //   checkWinX = true;
-  // };
+  //Column 1 X
+  if ($(".col1.playedX").length === 3) {
+    checkWinX = true;
+    roundInPlay = false;
+  };
+  //Column 1 O
+  if ($(".col1.playedO").length === 3) {
+    checkWinO = true;
+    roundInPlay = false;
+  };
+  //Column 2 X
+  if ($(".col2.playedX").length === 3) {
+    checkWinX = true;
+    roundInPlay = false;
+  };
   //Column 2 O
-  // if ($(".col2.playedO").length === 3) {
-  //   checkWinO = true;
-  // };
-  // //Column 3 X
-  // if ($(".col3.playedX").length === 3) {
-  //   checkWinX = true;
-  // };
+  if ($(".col2.playedO").length === 3) {
+    checkWinO = true;
+    roundInPlay = false;
+  };
+  //Column 3 X
+  if ($(".col3.playedX").length === 3) {
+    checkWinX = true;
+    roundInPlay = false;
+  };
   //Column 3 O
   if ($(".col3.playedO").length === 3) {
     checkWinO = true;
+    roundInPlay = false;
   };
 
-  //Column1
-  //Column2
-  //Column3
+  //Diag1 X
+  if ($(".diag1.playedX").length === 3) {
+    checkWinX = true;
+    roundInPlay = false;
+  };
+  //Diag1 O
+  if ($(".diag1.playedO").length === 3) {
+    checkWinO = true;
+    roundInPlay = false;
+  };
 
-  //Diag1
-  //Diag2
+  //Diag2 X
+  if ($(".diag2.playedX").length === 3) {
+    checkWinX = true;
+    roundInPlay = false;
+  };
+  //Diag2 O
+  if ($(".diag2.playedO").length === 3) {
+    checkWinO = true;
+    roundInPlay = false;
+  };
+
+  //Check if it's a draw
+  if (turnCounter === 9 && !checkWinX && !checkWinO) {
+    checkDraw = true;
+    roundInPlay = false;
+  };
+
 
 };
 
-// // Print the result to screen
-// const printResult = function () {
-//   if (checkWinX ) {
-//
-//   };
-// };
+// Print the result to screen
+const printResult = function () {
+  if (checkWinX ) {
+    $("#winner").css("visibility", "visible");
+    console.log("X Wins");
+  } else if (checkWinO) {
+    $("#winner").css("visibility", "visible");
+    console.log("O Wins");
+  };
+
+  if (checkDraw) {
+    $("#drawimg").css("visibility", "visible");
+    console.log("CAT'S GAME");
+  };
+};
 
 // const baloon = $('#baloon');
 // function runIt() {
@@ -107,11 +137,6 @@ const checkWinOrDraw = function () {
 //
 //
 
-const winnerImage = function () {
-  //if checkWinX --> display X winner image
-  //if checkWinO --> display O winner image
-  //else display draw image
-};
 //Is the row complete?
 
 $(document).ready(function () {
@@ -121,14 +146,14 @@ $(document).ready(function () {
     //If it is player 1's turn
     // debugger;
     empty(this);
-    if (player1 && squareEmpty) {
+    if (player1 && squareEmpty && roundInPlay) {
       //If the square hasn't been clicked yet
         //add class of x for player 1
         $(this).addClass("x playedX");
         player1 = false;
         turnCounter++;
 
-    } else if (!player1 && squareEmpty) {
+    } else if (!player1 && squareEmpty && roundInPlay) {
         //add class of o for player 2
         $(this).addClass("o playedO");
         player1 = true;
@@ -136,6 +161,7 @@ $(document).ready(function () {
     };
 
     checkWinOrDraw();
+    printResult();
   });
 
   $(".player1").on("click", function () {
